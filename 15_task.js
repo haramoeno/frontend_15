352a40d3c2d70d3a9172e25e9244e4da61c3dda2
haramoeno <moe.piece1201@ezweb.ne.jp>
2024-08-25 08:58:23 +0000

    課題の再提出

--- 15_task.js
+++ 15_task.js
@@ -1,9 +1,10 @@
 class Item {
-    constructor(id, name, used) {
+    constructor(id, name, stock, used) {
         
         this.id = id;
-        this.name = name
-        this,used = used;
+        this.name = name;
+        this.stock = stock;
+        this.used = used;
     }
     
     
@@ -15,16 +16,34 @@ class Item {
         return this.name;
     }
     
+    getStock() {
+        return this.stock;
+    }
+    
     getUsed() {
         return this.used;
+    }
+    
+    addStock(num) {
+        this.stock += num;
+    }
+    
+    
+    sale(num) {
+        if (this.stock < num) {
+          throw new Error('在庫が足りません')
+        } else {
+          this.stock -= num;
+        }
+      
     }
 }
 
 class Chair extends Item {
-    constructor(id, name, used) {
+    constructor(id, name, stock, used) {
         
-        super(id, name, used);
-        this.used = used;
+        super(id, name, stock, used);
+        
     }
     
     getUsed() {
@@ -33,9 +52,9 @@ class Chair extends Item {
 
 }
 
-let chair1 = new Chair('0001', 'デスクチェア', 'used');
-let chair2 = new Chair('0002', 'ダイニングチェア', 'used');
-let chair3 = new Chair('0003', '子ども用チェア', 'new');
+let chair1 = new Chair('0001', 'デスクチェア', '30', 'used');
+let chair2 = new Chair('0002', 'ダイニングチェア', '50', 'used');
+let chair3 = new Chair('0003', '子ども用チェア', '20', 'new');
 let item4 = new Item('0004', '鍋', 100);
 
 let itemList = [chair1, chair2, chair3, item4]
